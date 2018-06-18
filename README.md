776d75b6b1219bfeda81b6b61c69c52483dcbfbd
Dylan Hepworth <dylanhepworth@gmail.com>
2018-06-17 19:20:22 -0700

    Initial commit... after years of sitting on my computer

---
+++ README.md
@@ -0,0 +1,20 @@
+#SHADOWCERY
+##A jQuery plugin for making ridiculous shadows
+Shadowcery adds layered box-shadows to dom elements and animates them into view.
+
+Simply add jquery.shadowcery.js or jquery.shadowcery.min.js to your website (make sure jQuery is in place of course).
+
+In order to add shadows to an object, add the following:
+
+```
+$('.shadow').shadowcery();
+```
+
+###Options:
+color: Sets the color of the shadow
+opacity: Sets the opacity value from 0 to 1
+length: The total length of the shadow
+delay: The amount of time you'd like to delay the shadow from animating
+vertical: Sets the vertical direction of the shadow
+horizontal: Set the horizontal direction of the shadow
+blur: Amount of blur
---
+++ jquery.shadowcery.js
@@ -0,0 +1,51 @@
+/*!
+ * Shadowcery (http://getbootstrap.com)
+ * Copyright 2016 Dylan Hepworth (dylanhepworth.com)
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)*/
+(function($) {
+    $.fn.shadowcery = function(options) {
+    
+    var settings = $.extend({
+        color: "#838383",
+        horizontal: 1,
+        vertical: 1,
+        opacity: 0.8,
+        blur: 0.3,
+        length: 20,
+        delay: 0
+    }, options);
+    
+    var domText = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P', 'SPAN'],
+        shadowColor = hexRgb(settings.color),
+        css = "";
+    
+    if (domText.indexOf(this[0].tagName) != -1)
+        var shadowType = "text-shadow";
+    else
+        var shadowType = "box-shadow";
+    
+    function hexRgb(color) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
+    for (i = 1, h = settings.horizontal, v = settings.vertical, k = settings.opacity; i <= settings.length; i++, h = h + settings.horizontal, v = v + settings.vertical, k = k - 0.014) {
+        if (k <= 0)
+            k = 0.01;
+
+        if (i == settings.length)
+            var css = css + h + "px " + v + "px " + settings.blur + "px rgba(" + shadowColor['r'] + "," + shadowColor['g'] + "," + shadowColor['b'] + "," + k + ")"; 
+        else
+            var css = css + h + "px " + v + "px " + settings.blur + "px rgba(" + shadowColor['r'] + "," + shadowColor['g'] + "," + shadowColor['b'] + "," + k + "),"; 
+    }
+
+    if (shadowType == "text-shadow")
+        return this.delay(settings.delay).css({ "text-shadow":  css });    
+    else (shadowType == "box-shadow")
+        return this.delay(settings.delay).css({ "box-shadow":  css });    
+    };
+}(jQuery));
